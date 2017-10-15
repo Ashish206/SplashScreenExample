7a394bfa08abed6751c1f9d8773bc65e032ae736
Ashish Kushwaha <ashishkushwaha206@gmail.com>
2017-10-15 23:34:09 +0530

    Initial commit

---
+++ app/src/main/java/droidtuts4you/com/splashscreenexample/MainActivity.java
@@ -0,0 +1,13 @@
+package droidtuts4you.com.splashscreenexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/droidtuts4you/com/splashscreenexample/SplashAcivity.java
@@ -0,0 +1,26 @@
+package droidtuts4you.com.splashscreenexample;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashAcivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashAcivity.this,MainActivity.class);
+                SplashAcivity.this.startActivity(mainIntent);
+                SplashAcivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
